c59a910d0dde7e6245678d09f956986179917065
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-07-23 22:39:23 +0530

    index

---
+++ src/errors/index.ts
@@ -0,0 +1,9 @@
+export {
+  XeroError,
+  xeroErrorFromResponse,
+  type XeroErrorType,
+  type XeroErrorDetail,
+  type XeroErrorOptions,
+  type XeroValidationElement,
+  type XeroValidationMessage,
+} from "./XeroError.js";
